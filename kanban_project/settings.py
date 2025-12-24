ef6c3ade702980da3b8222b0986aa1fe7020ba2c
HJ-jyo <jyo1222lucky90@gmail.com>
2025-12-24 12:49:06 +0900

    .envから読み取るように修正

--- kanban_project/settings.py
+++ kanban_project/settings.py
@@ -9,25 +9,25 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import environ
 import os
 from django.utils.translation import gettext_lazy as _
 from pathlib import Path
 
+
+env = environ.Env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env.read_env(os.path.join(BASE_DIR, '.env'))
+SECRET_KEY = env('SECRET_KEY')
+DEBUG = env.bool('DEBUG', default=False)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e^9v7pu#@+8pryn%jvcx+54gef8oi7t+v=xj(ocjwabx$xc^1#'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
